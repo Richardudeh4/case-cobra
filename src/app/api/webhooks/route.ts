a5e10c47766caff7009b3579233d5015121266b3
richardudeh <richardudeh03@gmail.com>
2024-06-25 07:28:02 +0100

    fourth commit

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -1,7 +1,5 @@
 import { stripe } from "@/lib/stripe"
 import { db } from "@/src/db"
-
-
 import { headers } from "next/headers"
 import { NextResponse } from "next/server"
 import Stripe from "stripe"
@@ -15,9 +13,10 @@ export async function POST(req: Request){
             return new Response('Invalid signature', { status: 400 })
         }
         const event = stripe.webhooks.constructEvent(body, signature, process.env.STRIPE_WEBHOOK_SECRET!)
+
         if(event.type === "checkout.session.completed"){
             if(!event.data.object.customer_details?.email){
-                throw new Error('Missing user email ')
+                throw new Error('Missing user email')
             }
             const session = event.data.object as Stripe.Checkout.Session
 
@@ -31,7 +30,7 @@ export async function POST(req: Request){
             const billingAddress =  session.customer_details!.address
             const shippingAddress = session.shipping_details!.address 
 
-            await db.order.update({
+         await db.order.update({
                 where: {
                     id: orderId,
                 },
@@ -42,7 +41,7 @@ export async function POST(req: Request){
                             name: session.customer_details!.name!,
                             city: shippingAddress!.city!,
                             country : shippingAddress!.country!,
-                            postalCode : shippingAddress!.postal_code!,
+                            postalCode: shippingAddress!.postal_code!,
                             street: shippingAddress!.line1!,
                             state: shippingAddress!.state,
                         },
